a28efd9deba5bd9d94bba032bdcc8e4f07c48646
ahaskic1 <ahaskic1@eth.unsa.ba>
2018-11-01 18:28:39 +0100

    vjezba 1

---
+++ HelloWord.java
@@ -0,0 +1,20 @@
+package com.haskic.vjezba;
+
+import java.util.Scanner;
+
+public class HelloWord {
+
+    public static void main(String[] args) {
+        int n;
+        Scanner ulaz=new Scanner(System.in);
+        System.out.println("Unesite broj n: ");
+        n=ulaz.nextInt();
+        for(int i=1;i<=n;i++)
+            if (n % i == 0) {
+                System.out.print(i);
+                System.out.print(" ");
+            }
+        System.out.println("Selamalejk");
+	// write your code here
+    }
+}
